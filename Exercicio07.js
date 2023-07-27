b7c20d39c2f6dd414bebc0550f727786c75a96b7
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-07-27 14:19:14 -0300

    Exercicio07

---
+++ Exercicio07.js
@@ -0,0 +1,10 @@
+//Crie um programa que imprima os números de 1 a 10 em ordern crescente.
+
+const prompt = require('prompt-sync')()
+
+let contador = 1
+
+
+for(let i=1; i<11; i++){
+    console.log(i)
+}
