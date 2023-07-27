da569b8cfaf6ede842a160f3e96d744624be1208
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-07-27 14:00:52 -0300

    exercicio 05

---
+++ Exercicio05.js
@@ -0,0 +1,11 @@
+//Faça um programa que verifique se um número é par ou impar.
+
+const prompt = require('prompt-sync')()
+
+var numero = prompt("Digite um numeto: ")
+
+if(numero % 2 === 0){
+    console.log("O número é Par.")
+} else {
+    console.log("O número é Ímpar")
+}
