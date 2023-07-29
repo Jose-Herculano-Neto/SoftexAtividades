f22cbc7d40895f62d36598b500a231a7cf203763
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-07-29 14:37:13 -0300

    Exercicio10

---
+++ Exercicio09.js
@@ -0,0 +1,16 @@
+//Crie um programa que determine se um número é primo ou não.
+
+const prompt = require("prompt-sync")();
+
+let numero = prompt("Digite um número: ");
+
+function numPrimo(num){
+    for(i=2; i < num; i++){
+        if(num % i === 0){
+            return(console.log(`O número ${num} não é primo.`));
+        }
+    }
+    console.log(`O número ${num} é primo.`)
+}
+
+numPrimo(numero);
---
+++ Exercicio10.js
@@ -0,0 +1,12 @@
+//Faça um programa que converte graus Celsius para Fahrenheit
+
+const prompt = require("prompt-sync")();
+
+let temperaturaGraus = prompt("Digite qual a temperatura em C° que deseja converter: ")
+
+function converteTemperatura(temp){
+    let conversao = (temp * 1.8 + 32).toFixed(2);
+    console.log(`Temperatura em Fahrenheit: ${conversao}°`);
+}
+
+converteTemperatura(temperaturaGraus);
