917050c16f3ac8e0d318d202223d1053d76a941d
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-08-07 10:36:02 -0300

    Exercicio 14

---
+++ Exercicio13.js
@@ -0,0 +1,2 @@
+//Faça um programa que verifique se uma palavra é um palindromo (se pode ser lide da mesma forma de trás para frente).
+
---
+++ Exercicio14.js
@@ -0,0 +1,10 @@
+//Crie um programa que inverta a ordem dos elementos em uma lista.
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+function inverteLista(lista){
+    let listaInversa = lista.reverse()
+    console.log(listaInversa)
+}
+
+inverteLista(list)
