91971543b2cd23a7146580873e93dadc6a499d14
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-08-07 19:57:08 -0300

    Exercicio13

--- Exercicio13.js
+++ Exercicio13.js
@@ -1,2 +1,14 @@
 //Faça um programa que verifique se uma palavra é um palindromo (se pode ser lide da mesma forma de trás para frente).
 
+const palavra = "ovo"; // Faça o uso de outras palavras para testar
+
+function ehPalindromo(str) {
+  const reverso = str.split("").reverse().join("");
+  return str === reverso;
+}
+
+if (ehPalindromo(palavra)) {
+  console.log(`A palavra ${palavra} é um palíndromo!`);
+} else {
+  console.log(`A palavra ${palavra} não é um palíndromo.`);
+}
