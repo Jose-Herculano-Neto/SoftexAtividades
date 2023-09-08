844ca3caddf5e6afe624a2f04c5558f749ce3ba6
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-09-08 16:35:42 -0300

    calculadoraSoftex

---
+++ calculadoraSoftex.js
@@ -0,0 +1,21 @@
+
+function ola() {
+    console.log("Hello, World!");
+}
+
+
+function somar(a, b) {
+    return a + b;
+}
+
+
+const multiplicar = (a, b) => a * b;
+
+
+ola();
+
+const resultadoSoma = somar(8, 5);
+console.log("Resultado da soma:", resultadoSoma);
+
+const resultadoMultiplicacao = multiplicar(3, 4); 
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
